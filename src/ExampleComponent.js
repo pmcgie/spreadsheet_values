3bdce98be9751d89c27e21a8d08e98381835a375
Paul M <paul-mcgie@idexx.com>
2025-11-13 18:28:19 -0500

    Update ExampleComponent.js

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -1,12 +1,9 @@
-import { licenseKey } from '../config.json';
 import React, { useEffect, useState, useRef } from 'react';
-import './styles.css';
-import { HotTable, HotColumn } from "@handsontable/react";
-import "handsontable/dist/handsontable.min.css";
+import { HotTable, HotColumn } from '@handsontable/react';
+import 'handsontable/dist/handsontable.min.css';
 import { registerPlugin, AutoColumnSize, Autofill, ColumnSummary, ColumnSorting, ManualColumnFreeze, ContextMenu, DropdownMenu, UndoRedo } from 'handsontable/plugins';
 import { HyperFormula } from 'hyperformula';
 import { applyGrand, applyRow, applySub, dataToRows } from './helpers';
-import { debounce } from 'lodash';
 
 // Register plugins
 registerPlugin(AutoColumnSize);
@@ -18,41 +15,32 @@ registerPlugin(ContextMenu);
 registerPlugin(DropdownMenu);
 registerPlugin(UndoRedo);
 
-// HyperFormula instance
+// HyperFormula setup
 const hf = HyperFormula.buildEmpty({ licenseKey: 'internal-use-in-handsontable' });
 const sheetName = hf.addSheet("main");
 const sheetId = hf.getSheetId(sheetName);
 
-const ExampleSpreadsheet = ({ model, modelUpdate }) => {
+const ExampleSpreadsheetIframeUpdated = () => {
   const [formattedData, setFormattedData] = useState([]);
   const hotRef = useRef(null);
   const loadingRef = useRef(false);
+  const modelRef = useRef(null);
 
   // Track all user-edited cells
   const userChangesRef = useRef({});
 
-  // Debounced auto-commit (batch rapid edits)
-  const autoCommit = useRef(
-    debounce(() => {
-      if (Object.keys(userChangesRef.current).length > 0) {
-        modelUpdate({ updated_data: Object.values(userChangesRef.current) });
-      }
-    }, 150)
-  ).current;
+  // Flush editor to ensure last value is captured
+  const flushEditor = () => {
+    const hotInstance = hotRef.current?.hotInstance;
+    if (hotInstance?.getActiveEditor?.()) hotInstance.getActiveEditor().finishEditing(false);
+  };
 
-  // Initialize table when model.data changes
-  useEffect(() => {
-    if (model.data?.length) {
-      initializeTable(model.data);
-      userChangesRef.current = {};
-      modelUpdate({ updated_data: [] });
-    }
-  }, [model.data]);
-
-  const initializeTable = (data) => {
+  // Initialize table
+  const initializeTable = (model) => {
+    modelRef.current = model;
     loadingRef.current = true;
 
-    let formatted = dataToRows(data, model.pivot, model.groups, model.value, model.id);
+    let formatted = dataToRows(model.data, model.pivot, model.groups, model.value, model.id);
 
     if (model.totals && formatted?.data?.length) {
       if (model.totals.row_total) formatted = applyRow(formatted);
@@ -64,19 +52,13 @@ const ExampleSpreadsheet = ({ model, modelUpdate }) => {
 
     setTimeout(() => {
       if (hotRef.current) hotRef.current.loadData(formatted.data);
-      try { hf.clearSheet(sheetId); } catch (e) {}
+      try { hf.clearSheet(sheetId); } catch(e) {}
       hf.setSheetContent(sheetId, formatted.data);
       loadingRef.current = false;
     }, 0);
   };
 
-  // Commit in-progress editor
-  const flushEditor = () => {
-    const hotInstance = hotRef.current?.hotInstance;
-    if (hotInstance?.getActiveEditor?.()) hotInstance.getActiveEditor().finishEditing(false);
-  };
-
-  // AfterChange: mark dirty cells and debounce commit
+  // AfterChange: report last cell and update all edited cells
   const afterChange = (changes, type) => {
     if (!changes?.length || type === 'loadData' || loadingRef.current) return;
     const relevantTypes = ['edit','Autofill.fill','CopyPaste.cut','CopyPaste.paste'];
@@ -84,40 +66,28 @@ const ExampleSpreadsheet = ({ model, modelUpdate }) => {
 
     flushEditor();
 
-    changes.forEach(([rowIndex, colIndex, oldValue, newValue]) => {
-      const colName = formattedData.columns[colIndex];
-      const rowId = formattedData.data[rowIndex][model.id];
+    // Only the last change
+    const [lastChange] = [changes[changes.length - 1]];
+    const [rowIndex, colIndex, oldValue, newValue] = lastChange;
 
-      if (!userChangesRef.current[rowId]) userChangesRef.current[rowId] = {};
-      userChangesRef.current[rowId][colName] = newValue;
-      userChangesRef.current[rowId][model.id] = rowId;
-    });
+    const colName = formattedData.columns[colIndex];
+    const rowId = formattedData.data[rowIndex][modelRef.current.id];
 
-    // Auto-commit changes after debounce
-    autoCommit();
+    // Update tracking
+    if (!userChangesRef.current[rowId]) userChangesRef.current[rowId] = {};
+    userChangesRef.current[rowId][colName] = newValue;
+    userChangesRef.current[rowId][modelRef.current.id] = rowId;
+
+    const lastChangedCell = { [modelRef.current.id]: rowId, [colName]: newValue };
+    const allUpdatedData = Object.values(userChangesRef.current);
+
+    if (window.parent) {
+      window.parent.postMessage({ type: 'CELL_CHANGED', cell: lastChangedCell }, '*');
+      window.parent.postMessage({ type: 'UPDATED_DATA', updated_data: allUpdatedData }, '*');
+    }
   };
 
-  // Safely apply formulas/totals without overwriting user-edited cells
-  const applyFormulasSafely = (hotData) => {
-    return hotData.map((row, rowIndex) => {
-      const rowId = row[model.id];
-      const newRow = [...row];
-
-      formattedData.columns.forEach((colName, colIndex) => {
-        // Only overwrite if user hasn't edited this cell
-        if (!userChangesRef.current[rowId]?.[colName]) {
-          try {
-            const hfValue = hf.getCellValue(sheetId, rowIndex, colIndex);
-            if (hfValue !== undefined) newRow[colIndex] = hfValue;
-          } catch (e) {}
-        }
-      });
-
-      return newRow;
-    });
-  };
-
-  // Highlight dirty/user-edited cells
+  // Highlight user-edited cells
   const columnSummaryStyle = (row, col) => {
     const classNames = [];
 
@@ -125,49 +95,57 @@ const ExampleSpreadsheet = ({ model, modelUpdate }) => {
     if (formattedData.row_total_column === col) classNames.push('row_total');
     if (formattedData.sub_total_rows?.includes(row)) classNames.push('sub_total');
 
-    const rowId = formattedData.data?.[row]?.[model.id];
+    const rowId = formattedData.data?.[row]?.[modelRef.current.id];
     const colName = formattedData.columns[col];
-    if (rowId && userChangesRef.current[rowId]?.[colName] !== undefined) {
-      classNames.push('dirty_cell');
-    }
+    if (rowId && userChangesRef.current[rowId]?.[colName] !== undefined) classNames.push('dirty_cell');
 
     return classNames.length ? { className: classNames.join(' '), readOnly: false } : {};
   };
 
+  // Listen for model from parent
+  useEffect(() => {
+    const handler = (event) => {
+      if (event.data?.type === 'SET_MODEL') {
+        initializeTable(event.data.model);
+        userChangesRef.current = {};
+      }
+    };
+    window.addEventListener('message', handler);
+    return () => window.removeEventListener('message', handler);
+  }, []);
+
   if (!formattedData?.data?.length) return <></>;
 
   return (
-    <div style={{ height: '100vh', width: '100vw', position: 'relative' }}>
-      <HotTable
-        ref={hotRef}
-        data={formattedData.data}
-        colHeaders={formattedData.columns.map(c => model.labels ? model.labels[model.fields.indexOf(c)] || c : c)}
-        columnSorting={Boolean(model.columnSorting)}
-        undoRedo={true}
-        contextMenu={Boolean(model.contextMenu)}
-        manualColumnFreeze={model.fixedColumnsLeft && Number(model.fixedColumnsLeft) > 0}
-        fixedColumnsLeft={model.fixedColumnsLeft && Number(model.fixedColumnsLeft) > 0 ? model.fixedColumnsLeft : 0}
-        colWidths={model.colWidths}
-        fillHandle={{ autoInsertRow: false, autoInsertColumn: false }}
-        allowInsertRow={false}
-        allowInsertColumn={false}
-        cells={columnSummaryStyle}
-        afterChange={afterChange}
-        formulas={{ engine: hf, sheetName }}
-      >
-        {formattedData.columns.map((c, i) =>
-          c !== '_ids' ? (
-            <HotColumn
-              key={c}
-              data={i}
-              readOnly={model.groups.indexOf(c) > -1}
-              type={model.groups.indexOf(c) > -1 ? 'numeric' : 'text'}
-            />
-          ) : <React.Fragment key={c} />
-        )}
-      </HotTable>
-    </div>
+    <HotTable
+      ref={hotRef}
+      data={formattedData.data}
+      colHeaders={formattedData.columns.map(c => modelRef.current?.labels ? modelRef.current.labels[modelRef.current.fields.indexOf(c)] || c : c)}
+      columnSorting={Boolean(modelRef.current?.columnSorting)}
+      undoRedo={true}
+      contextMenu={Boolean(modelRef.current?.contextMenu)}
+      manualColumnFreeze={modelRef.current?.fixedColumnsLeft && Number(modelRef.current.fixedColumnsLeft) > 0}
+      fixedColumnsLeft={modelRef.current?.fixedColumnsLeft && Number(modelRef.current.fixedColumnsLeft) > 0 ? modelRef.current.fixedColumnsLeft : 0}
+      colWidths={modelRef.current?.colWidths}
+      fillHandle={{ autoInsertRow: false, autoInsertColumn: false }}
+      allowInsertRow={false}
+      allowInsertColumn={false}
+      cells={columnSummaryStyle}
+      afterChange={afterChange}
+      formulas={{ engine: hf, sheetName }}
+    >
+      {formattedData.columns.map((c, i) =>
+        c !== '_ids' ? (
+          <HotColumn
+            key={c}
+            data={i}
+            readOnly={modelRef.current?.groups?.indexOf(c) > -1}
+            type={modelRef.current?.groups?.indexOf(c) > -1 ? 'numeric' : 'text'}
+          />
+        ) : <React.Fragment key={c} />
+      )}
+    </HotTable>
   );
 };
 
-export default ExampleSpreadsheet;
+export default ExampleSpreadsheetIframeUpdated;
